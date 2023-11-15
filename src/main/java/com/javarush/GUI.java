8e79a72adc36a1df32dae6dca358cc63e317fd1c
max0st <maksi>
2023-11-15 16:58:43 +0200

    Added new methods `showErrorDialog` and `showAccesDialog` to inform the user whether the programme worked successfully or not Added new labels

--- src/main/java/com/javarush/GUI.java
+++ src/main/java/com/javarush/GUI.java
@@ -2,23 +2,21 @@
  * The `GUI` class provides a simple JavaFX graphical user interface (GUI) for interacting with the file
  * encryption and decryption operations defined in the `FileService` class. It allows users to select a command,
  * enter file paths, keys, and perform actions through a graphical interface.
- *
+ * <p>
  * This class includes the following methods:
- *
+ * <p>
  * 1. `launchGUI()`: Launches the GUI application.
- *
+ * <p>
  * 2. `main(String[] args)`: The main method to launch the GUI application.
- *
+ * <p>
  * 3. `showErrorDialog(String message)`: Displays an error dialog with the specified message.
- *
+ * <p>
  * 4. `showSuccessDialog(String operation)`: Displays a success dialog for the specified operation.
- *
+ * <p>
  * 5. `showSuccessDialog(String operation, int key)`: Displays a success dialog for the specified operation with the
- *    probable key.
- *
+ * probable key.
+ * <p>
  * 6. `start(Stage primaryStage)`: Initializes and sets up the JavaFX GUI components.
- *
- *
  */
 
 package com.javarush;
