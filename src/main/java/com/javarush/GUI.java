f45adef72e5c392925308c39e44170ea1f3a6349
max0st <maksi>
2023-11-15 16:55:29 +0200

    Added new methods `showErrorDialog` and `showAccesDialog` to inform the user whether the programme worked successfully or not Added new labels

--- src/main/java/com/javarush/GUI.java
+++ src/main/java/com/javarush/GUI.java
@@ -1,3 +1,26 @@
+/**
+ * The `GUI` class provides a simple JavaFX graphical user interface (GUI) for interacting with the file
+ * encryption and decryption operations defined in the `FileService` class. It allows users to select a command,
+ * enter file paths, keys, and perform actions through a graphical interface.
+ *
+ * This class includes the following methods:
+ *
+ * 1. `launchGUI()`: Launches the GUI application.
+ *
+ * 2. `main(String[] args)`: The main method to launch the GUI application.
+ *
+ * 3. `showErrorDialog(String message)`: Displays an error dialog with the specified message.
+ *
+ * 4. `showSuccessDialog(String operation)`: Displays a success dialog for the specified operation.
+ *
+ * 5. `showSuccessDialog(String operation, int key)`: Displays a success dialog for the specified operation with the
+ *    probable key.
+ *
+ * 6. `start(Stage primaryStage)`: Initializes and sets up the JavaFX GUI components.
+ *
+ *
+ */
+
 package com.javarush;
 
 import com.javarush.fileoperations.FileService;
@@ -10,6 +33,9 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
+    protected static void launchGUI() {
+        Application.launch(GUI.class);
+    }
 
     private static void showErrorDialog(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -21,6 +47,22 @@ public class GUI extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void showSuccessDialog(String operation) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Success");
+        alert.setHeaderText(null);
+        alert.setContentText(operation + " completed successfully!");
+        alert.showAndWait();
+    }
+
+    private void showSuccessDialog(String operation, int key) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Success");
+        alert.setHeaderText(null);
+        alert.setContentText(operation + " completed successfully! Probable key is" + key);
+        alert.showAndWait();
     }
 
     @Override
@@ -53,7 +95,14 @@ public class GUI extends Application {
 
         Label infoLabel = new Label("If the field is empty,\n the default letter frequencies will be used");
         infoLabel.setVisible(false);
+
         Label infoForCommand = new Label("Choose the command to perform actions");
+
+        Label infoKey = new Label("Provide the key:");
+        infoKey.setVisible(false);
+
+        Label infoForPath = new Label("Browse the path to the file.txt that you want to work with:");
+        infoForPath.setVisible(false);
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Choose File");
@@ -77,7 +126,6 @@ public class GUI extends Application {
             String key = keyField.getText();
             String filePathForStaticAnalysis = filePathForStaticAnalysisField.getText();
 
-            // Перевірка введених даних
             if (command == null || command.isEmpty()) {
                 showErrorDialog("Please select a command.");
                 return;
@@ -98,7 +146,7 @@ public class GUI extends Application {
                 return;
             }
 
-            if (!FileService.fileExists(filePathForStaticAnalysis)) {
+            if ((command.equalsIgnoreCase("BRUTE_FORCE")) && !FileService.fileExists(filePathForStaticAnalysis) && !filePathForStaticAnalysis.isEmpty()) {
                 showErrorDialog("File for static analysis not found at the specified path.");
                 return;
             }
@@ -106,20 +154,27 @@ public class GUI extends Application {
             switch (command) {
                 case "ENCRYPT":
                     FileService.encryptFile(filePath, Integer.parseInt(key));
+                    showSuccessDialog(command);
                     break;
                 case "DECRYPT":
                     FileService.decryptFile(filePath, Integer.parseInt(key));
+                    showSuccessDialog(command);
                     break;
                 case "BRUTE_FORCE":
+                    int decryptedKey;
                     if (!filePathForStaticAnalysis.isEmpty()) {
-                        FileService.decryptAnalysisBF(filePath, filePathForStaticAnalysis);
+                        decryptedKey = FileService.decryptAnalysisBF(filePath, filePathForStaticAnalysis);
                     } else {
-                        FileService.decryptBF(filePath);
+                        decryptedKey = FileService.decryptBF(filePath);
                     }
+                    showSuccessDialog(command, decryptedKey);
+
                     break;
                 default:
                     showErrorDialog("Invalid command.");
             }
+
+
         });
 
 
@@ -131,6 +186,7 @@ public class GUI extends Application {
             filePathField.setVisible(true);
             keyLabel.setVisible(!isBruteForce);
             keyField.setVisible(!isBruteForce);
+            infoKey.setVisible(!isBruteForce);
             filePathForStaticAnalysisLabel.setVisible(isBruteForce);
             filePathForStaticAnalysisField.setVisible(isBruteForce);
             infoLabel.setVisible(isBruteForce);
@@ -138,37 +194,36 @@ public class GUI extends Application {
             browseFilePathButton.setVisible(true);
             browseFilePathForStatAnalysisButton.setVisible(isBruteForce);
             infoForCommand.setVisible(false);
+            infoForPath.setVisible(true);
 
+            // Remove unnecessary nodes before adding new ones
+            grid.getChildren().removeAll(keyLabel, keyField, infoKey, filePathForStaticAnalysisLabel, filePathForStaticAnalysisField, browseFilePathForStatAnalysisButton);
 
             if (isBruteForce) {
-                grid.getChildren().removeAll(keyLabel, keyField);
-                grid.add(filePathForStaticAnalysisLabel, 0, 2);
-                grid.add(filePathForStaticAnalysisField, 1, 2);
+                grid.add(filePathForStaticAnalysisLabel, 0, 3);
+                grid.add(filePathForStaticAnalysisField, 1, 3);
+                grid.add(browseFilePathForStatAnalysisButton, 2, 3);
             } else {
-                grid.getChildren().removeAll(filePathForStaticAnalysisLabel, filePathForStaticAnalysisField, infoLabel);
-                grid.add(keyLabel, 0, 2);
-                grid.add(keyField, 1, 2);
+                grid.add(infoKey, 0, 3);
+                grid.add(keyLabel, 0, 4);
+                grid.add(keyField, 1, 4);
             }
         });
 
 
         grid.add(commandLabel, 0, 0);
         grid.add(commandComboBox, 1, 0);
-        grid.add(infoForCommand,1,1);
-        grid.add(filePathLabel, 0, 1);
-        grid.add(filePathField, 1, 1);
-        grid.add(browseFilePathButton, 2, 1);
-        grid.add(filePathForStaticAnalysisLabel, 0, 2);
-        grid.add(filePathForStaticAnalysisField, 1, 2);
-        grid.add(browseFilePathForStatAnalysisButton, 2 ,2);
-        grid.add(infoLabel, 3, 2);
-        grid.add(keyLabel, 0, 2);
-        grid.add(keyField, 1, 2);
-        grid.add(executeButton, 1, 4);
+        grid.add(infoForCommand, 1, 1);
+        grid.add(infoForPath, 0, 1);
+        grid.add(filePathLabel, 0, 2);
+        grid.add(filePathField, 1, 2);
+        grid.add(browseFilePathButton, 2, 2);
+        grid.add(infoLabel, 3, 3);
+        grid.add(executeButton, 1, 5);
 
         GridPane.setColumnSpan(infoLabel, GridPane.REMAINING);
 
-        Scene scene = new Scene(grid, 650, 250);
+        Scene scene = new Scene(grid, 900, 250);
         primaryStage.setScene(scene);
 
         primaryStage.show();
