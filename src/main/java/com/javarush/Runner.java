112e10a8ac16adbf8bd15d7986df20342eee679f
max0st <maksi>
2023-11-15 16:56:30 +0200

    Changed the second option of launching the programme to launching it using GUI

--- src/main/java/com/javarush/Runner.java
+++ src/main/java/com/javarush/Runner.java
@@ -6,7 +6,7 @@ public class Runner {
         if (args.length > 0) {
             CLI.runFromArgs(args);
         } else {
-            CLI.runFromCLI();
+            GUI.launchGUI();
         }
     }
 }
