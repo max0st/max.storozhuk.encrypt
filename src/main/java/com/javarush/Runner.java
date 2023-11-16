87c82b1a733deb8b16cbaf6112f63557a5447860
max0st <maksi>
2023-11-16 14:26:20 +0200

    Added an option to run in the CLI

--- src/main/java/com/javarush/Runner.java
+++ src/main/java/com/javarush/Runner.java
@@ -1,12 +1,23 @@
 package com.javarush;
-
+import java.util.Scanner;
 public class Runner {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
         if (args.length > 0) {
             CLI.runFromArgs(args);
         } else {
-            GUI.launchGUI();
+            System.out.println("Select the operating mode:");
+            System.out.println("1 - CLI");
+            System.out.println("2 - GUI");
+            System.out.print("Enter the number of the selected mode: ");
+            String choice = scanner.nextLine();
+            if (choice.equals("1")) {CLI.runFromCLI();}
+            else if(choice.equals("2")) {GUI.launchGUI();}
+            else {System.out.println("Incorrect command");}
+
         }
     }
 }
