648a25d3a6d8f024ab2c93b7c971f2b4df34e499
max0st <maksi>
2023-11-23 18:40:48 +0200

    Initialized CLI variable

--- src/main/java/com/javarush/Runner.java
+++ src/main/java/com/javarush/Runner.java
@@ -8,10 +8,10 @@ public class Runner {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        CLI cli = new CLI();
 
         if (args.length > 0) {
-            CLI.runFromArgs(args);
+            cli.runFromArgs(args);
         } else {
             System.out.println("Select the operating mode:");
             System.out.println("1 - CLI");
@@ -19,7 +19,7 @@ public class Runner {
             System.out.print("Enter the number of the selected mode: ");
             String choice = scanner.nextLine();
             if (choice.equals("1")) {
-                CLI.runFromCLI();
+                cli.runFromCLI();
             } else if (choice.equals("2")) {
                 new GUI().launchGUI();
             } else {
