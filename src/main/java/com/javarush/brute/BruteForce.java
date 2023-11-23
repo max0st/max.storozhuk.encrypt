d0d4cfc1e5fda0bfdabce2610507e461d96dbae0
max0st <maksi>
2023-11-23 18:36:14 +0200

    Removed static keywords from methods

--- src/main/java/com/javarush/brute/BruteForce.java
+++ src/main/java/com/javarush/brute/BruteForce.java
@@ -13,7 +13,7 @@
  * on the input encrypted text using static analysis of expected letter percentages from a specified file.
  * It returns the most plausible result along with the corresponding key.
  * <p>
- * 3. `private static int findKey(String encryptedText)`:Finds the most plausible key for brute force decryption based on frequency analysis.
+ * 3. `findKey(String encryptedText)`:Finds the most plausible key for brute force decryption based on frequency analysis.
  * It iterates through all possible keys and evaluates the plausibility of each decryption attempt.
  * <p>
  * 4. `findKey(String encryptedText, String filePathForStaticAnalysis)`: Finds the most plausible key for brute force decryption based on static analysis of expected
@@ -29,13 +29,14 @@
 package com.javarush.brute;
 
 import com.javarush.cipher.AlphabetDeterminer;
-
+import com.javarush.cipher.CaesarCipher;
 
 import java.util.Map;
 
-import static com.javarush.cipher.CaesarCipher.decrypt;
-
 public class BruteForce {
+    private final CaesarCipher caesarCipher = new CaesarCipher();
+    private final ExpectedPercentages expectedPercentages = new ExpectedPercentages();
+    private final FrequencyAnalysis frequencyAnalysis = new FrequencyAnalysis();
 
     /**
      * Performs brute force decryption on the input encrypted text and returns the most plausible result
@@ -45,9 +46,9 @@ public class BruteForce {
      * @param encryptedText The text to be decrypted.
      * @return The result of the brute force decryption, including the decrypted text and the decryption key.
      */
-    public static BruteForceResult bruteForceDecrypt(String encryptedText) {
+    public BruteForceResult bruteForceDecrypt(String encryptedText) {
         int mostPlausibleKey = findKey(encryptedText);
-        String decryptedText = decrypt(encryptedText, mostPlausibleKey);
+        String decryptedText = caesarCipher.decrypt(encryptedText, mostPlausibleKey);
         return new BruteForceResult(decryptedText, mostPlausibleKey);
     }
 
@@ -61,9 +62,9 @@ public class BruteForce {
      * @return The result of the brute force decryption, including the decrypted text and the decryption key.
      */
 
-    public static BruteForceResult bruteForceDecrypt(String encryptedText, String filePathForStaticAnalysis) {
+    public BruteForceResult bruteForceDecrypt(String encryptedText, String filePathForStaticAnalysis) {
         int mostPlausibleKey = findKey(encryptedText, filePathForStaticAnalysis);
-        String decryptedText = decrypt(encryptedText, mostPlausibleKey);
+        String decryptedText = caesarCipher.decrypt(encryptedText, mostPlausibleKey);
         return new BruteForceResult(decryptedText, mostPlausibleKey);
     }
 
@@ -74,14 +75,14 @@ public class BruteForce {
      * @param encryptedText The text to be decrypted.
      * @return The most plausible key for decryption.
      */
-    private static int findKey(String encryptedText) {
+    private int findKey(String encryptedText) {
         int alphabetLength = (AlphabetDeterminer.determineAlphabet(encryptedText).length()) - AlphabetDeterminer.getAdditionalCases().length();
         int bestKey = 0;
         double bestPlausibility = Double.POSITIVE_INFINITY;
 
         for (int key = 1; key < alphabetLength + 1; key++) {
-            String decryptedText = decrypt(encryptedText, key);
-            double plausibility = FrequencyAnalysis.calculateProbability(decryptedText);
+            String decryptedText = caesarCipher.decrypt(encryptedText, key);
+            double plausibility = frequencyAnalysis.calculateProbability(decryptedText);
 
             if (plausibility < bestPlausibility) {
                 bestPlausibility = plausibility;
@@ -101,16 +102,16 @@ public class BruteForce {
      * @return The most plausible key for decryption.
      */
 
-    private static int findKey(String encryptedText, String filePathForStaticAnalysis) {
+    private int findKey(String encryptedText, String filePathForStaticAnalysis) {
         int alphabetLength = (AlphabetDeterminer.determineAlphabet(encryptedText).length()) - AlphabetDeterminer.getAdditionalCases().length();
         int bestKey = 0;
         double bestPlausibility = Double.POSITIVE_INFINITY;
 
-        Map<Character, Double> letterPercentages = ExpectedPercentages.calculateLetterPercentages(filePathForStaticAnalysis);
+        Map<Character, Double> letterPercentages = expectedPercentages.calculateLetterPercentages(filePathForStaticAnalysis);
 
         for (int key = 1; key < alphabetLength + 1; key++) {
-            String decryptedText = decrypt(encryptedText, key);
-            double plausibility = FrequencyAnalysis.calculateProbability(decryptedText, letterPercentages);
+            String decryptedText = caesarCipher.decrypt(encryptedText, key);
+            double plausibility = frequencyAnalysis.calculateProbability(decryptedText, letterPercentages);
 
             if (plausibility < bestPlausibility) {
                 bestPlausibility = plausibility;
